318300753dee7c7abac3c07756960a336686062c
anne <anneirungu07@gmail.com>
2024-01-09 20:15:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (const name of names) {
+    thankYouMessages.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+}
+
+// Function to count down 
+function countDown(number) {
+    while (number >= 0) {
+    console.log(number);
+    number--;
+}
+}
